233f0fcd1d46f0b7f9c313c39f6ba92109807d81
Mohit <144538075+Mohit-Alive@users.noreply.github.com>
2024-08-07 10:42:55 +0530

    Create 03-transpose.c

---
+++ 03-transpose.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main() {
+    int arr[2][2] = {{1,2}, {3, 4}};
+    
+    printf("Transposed Matrix is: \n");
+    for(int i=0; i<2; i++){
+        for(int j=0; j<2;j++){
+            printf("%d ", arr[j][i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
